af9099fd582e7e73cc73c14511aab40f7ae649fc
krishnab <cyclotomiq@gmail.com>
2017-10-23 00:14:22 -0400

    added builder pattern structure for creating requests. Created the Director, ABC, and a concrete class.

---
+++ mining_data_acquisition/abcRequest.py
@@ -0,0 +1,64 @@
+
+#!/usr/bin/python
+
+"""
+Abstract Base Class for Request class
+
+
+"""
+
+## MIT License
+##
+## Copyright (c) 2017, krishna bhogaonker
+## Permission is hereby granted, free of charge, to any person obtaining a ## copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+## The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+## THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+__author__ = 'krishna bhogaonker'
+__copyright__ = 'copyright '
+__credits__ = ['krishna bhogaonker']
+__license__ = "MIT"
+__version__ = ''
+__maintainer__ = 'krishna bhogaonker'
+__email__ = 'cyclotomiq@gmail.com'
+__status__ = ''
+
+
+import abc
+
+class abcRequest(metaclass=abc.ABCMeta):
+
+    def __init__(self, successor=None):
+        if ValidationLogic.isSuccessor:
+            self._successor = successor
+
+
+
+    @abc.abstractmethod
+    def handle_request(self):
+        pass
+
+
+class ValidationLogic:
+
+    @classmethod
+    def isSuccessor(value):
+        if not isinstance(value, Handler):
+            raise(NotAHandler)
+
+
+
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+class NotAHandler(Error):
+    def __init__(self, evalue):
+        print('The value entered is not a handler: ' + str(evalue))
+
+
+if __name__ == "__main__":
+    print("This is an abstract base class. No functionality available here.")
